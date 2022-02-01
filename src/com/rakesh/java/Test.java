59a0c7033c7989dac2b08e1007adac0b682322ec
rakesh <rakesh.nayak346@gmail.com>
2022-02-01 12:11:41 +0530

    first commit

---
+++ src/com/rakesh/java/Test.java
@@ -0,0 +1,16 @@
+package com.rakesh.java;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Test {
+
+	public static void main(String[] args) {
+	Date d=new Date();
+	System.out.println(d.toString());
+	//char[] LocalDate;
+	System.out.println(LocalDate.now());
+
+	}
+
+}
